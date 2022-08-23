00cfb6a001c9741eea662fc7b971a01dd605d155
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-08-23 16:22:32 -0500

    Daño a enemigos,Score y Vida modificados

--- juego.js
+++ juego.js
@@ -4,11 +4,11 @@ let ctx = lienzo.getContext("2d")
 // Naves / Objetos
 const halconImg = new Image()
 halconImg.src = "emissary.png";//Halcon Milenario
-console.log(halconImg);
+
 
 const estrellaImg = new Image();
 estrellaImg.src = "enemigo.png"//Estrella de la Muerte
-console.log(estrellaImg);
+
 
 const balasImg = new Image();
 balasImg.src = "./balas1.png";//Balas Halcon
@@ -17,8 +17,8 @@ const disparoSonido = new Audio();
 disparoSonido.src = "./disparo.mp3";
 
 //Listado de Enemigos
-const enemy = [];
-const rafaga = [];
+const enemy = [];//enemigos
+const rafaga = [];//balas 
 //Creacion de naves
 class Nave{
 	constructor(x,y,w,h,color,imagen){
@@ -73,7 +73,7 @@ class Enemigo{
 		ctx.fillStyle = "black";
 		ctx.fillRect(this.x, this.y, this.w, this.h, this.image);
 		ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
-		this.y += .5;
+		this.y += 1;   //MOVIMIENTO DESCENDENTE
 	}
 
 }
@@ -81,7 +81,7 @@ class Enemigo{
 //Funcion para escuchar teclas 
 function teclas(milenario){
 	document.addEventListener("keydown",(evento)=>{
-		console.log("Tecla tocada: ",evento.code);
+		// console.log("Tecla tocada: ",evento.code);
 		switch(evento.code){
 			case"ArrowLeft":
 			milenario.izquierda()
@@ -134,21 +134,20 @@ class Balas {
 
 // Crear Enemigos
 function crearEnemigos(){
-	const num = Math.round(Math.random()*1000)
- if(num ===8){
-	 const estrella1 = new Enemigo(0,-50,100,100,"white",estrellaImg);
-	 const estrella2 = new Enemigo(100,-100,100,100,"white",estrellaImg);
-	 const estrella3= new Enemigo(200,-150,100,100,"white",estrellaImg);
-	 const estrella4 = new Enemigo(300,-200,100,100,"white",estrellaImg)
-	 const estrella5 = new Enemigo(400,-250,100,100,"white",estrellaImg)
-	 const estrella6 = new Enemigo(500,-300,100,100,"white",estrellaImg)
-	 const estrella7 = new Enemigo(600,-350,100,100,"white",estrellaImg)
-	 const estrella8 = new Enemigo(700,-400,100,100,"white",estrellaImg)
-	 const estrella9 = new Enemigo(800,-450,100,100,"white",estrellaImg)
-	 const estrella10 = new Enemigo(900,-500,100,100,"white",estrellaImg)
+	const num = Math.floor(Math.random()*1000)
+ if(num ===10){
+	 const estrella1 = new Enemigo(10,0,100,100,"white",estrellaImg);
+	 const estrella2 = new Enemigo(130,-200,100,100,"white",estrellaImg);
+	 const estrella3= new Enemigo(250,-400,100,100,"white",estrellaImg);
+	 const estrella4 = new Enemigo(370,-750,100,100,"white",estrellaImg)
+	 const estrella5 = new Enemigo(490,-800,100,100,"white",estrellaImg)
+	 const estrella6 = new Enemigo(610,-950,100,100,"white",estrellaImg)
+	 const estrella7 = new Enemigo(730,-1000,100,100,"white",estrellaImg)
+	 const estrella8 = new Enemigo(850,-1250,100,100,"white",estrellaImg)
+
 	 
 	 
-	enemy.push(estrella1,estrella2,estrella3,estrella4,estrella5,estrella6,estrella7,estrella8,estrella9,estrella10);
+	 enemy.push(estrella1,estrella2,estrella3,estrella4,estrella5,estrella6,estrella7,estrella8);
  }
 
 }  
@@ -163,54 +162,66 @@ function mostrarDatos(tiempo,score,vida){
 	ctx.fillText(`Vida: ${vida}`,15,80)
 } 
 
+//Colisiones
+// function colisiones(){
+// 	for(let i =0;enemy.length;i++){
+// 		for (let j=0;balas.length;j++){
+// 			malos = enemy[i];
+// 			municiones = rafaga[j];
+// 			if((municiones.x>malos.x)&& (municiones.x < malos.x+,alos.w))
+// 		}
+// 	}
+// }
+
 //Funcion para iniciar el juego
 function iniciarJuego(){
 	let tiempo = 0;
 	let score = 0;
 	let vida = 100;
-	const estrella1 = new Enemigo(0,0,100,100,"white",estrellaImg)
-	const estrella2 = new Enemigo(100,-50,100,100,"white",estrellaImg)
-	const estrella3 = new Enemigo(200,-150,100,100,"white",estrellaImg)
-	const estrella4 = new Enemigo(300,-200,100,100,"white",estrellaImg)
-	const estrella5 = new Enemigo(400,-250,100,100,"white",estrellaImg)
-	const estrella6 = new Enemigo(500,-300,100,100,"white",estrellaImg)
-	const estrella7 = new Enemigo(600,-350,100,100,"white",estrellaImg)
-	const estrella8 = new Enemigo(700,-400,100,100,"white",estrellaImg)
-	const estrella9 = new Enemigo(800,-450,100,100,"white",estrellaImg)
-	const estrella10 = new Enemigo(900,-500,100,100,"white",estrellaImg)
 	const milenario = new Nave(400,190,100,100,"black",halconImg);//halcon
 	teclas(milenario);
 	
-	
+
 	
 	
 	setInterval(()=>{
+		if(score >= 10){
+			alert("vientos")
+		}else if(vida <= 0){
+			alert("mamaste")
+		}
 		ctx.clearRect(0,0,1000,500);
 		tiempo +=1;
 		milenario.dibujarse();//milenario
-		estrella1.dibujarse();
-		estrella2.dibujarse();
-		estrella3.dibujarse();
-		estrella4.dibujarse();
-		estrella5.dibujarse();
-		estrella6.dibujarse();
-		estrella7.dibujarse();//estrella 
-		estrella8.dibujarse();
-		estrella9.dibujarse();
-		estrella10.dibujarse();
-		
-		rafaga.forEach((balas)=>{//Balas
-			balas.dibujarse();
-		})
-		enemy.forEach((enemigos)=>{
-			if(enemigos.y <= 100){
-				milenario.vida-=10;
-			}
-			enemigos.dibujarse();
-		})
 		crearEnemigos();
+
+		enemy.forEach((enemigos,index)=>{
+			enemigos.dibujarse();
+			// balas.dibujarse()
+			if(enemigos.y+enemigos.w>= 500){
+				vida -=10;
+				enemy.splice(index,1);
+			}
+		})
 		mostrarDatos(tiempo,score,vida);//mostrar datos
-	},1000/100);
+		rafaga.forEach((bala,bIndex)=>{//Balas
+			if(bala.y <= 0){
+				rafaga.splice(bIndex,1);
+			}
+			enemy.forEach((malo,mIndex)=>{
+				if(bala.y <= malo.y + malo.h &&
+					bala.x <= malo.x + malo.w &&
+					bala.x >= malo.x){
+					console.log("toca")
+					enemy.splice(mIndex,1);
+					rafaga.splice(bIndex,1);
+					score +=1;
+				}
+			})
+			bala.dibujarse();
+			
+		});
+	},1000/60);
 }
 		iniciarJuego();
 		
